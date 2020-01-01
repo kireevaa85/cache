5718fd8282d7dcc2892b4bf4457c9dda6582a59e
kireevaa85 <kireevaa85@gmail.com>
2020-01-02 02:03:16 +0300

    Cache. Ehcache

---
+++ src/main/java/ru/kireev/ehcache/SlowDataSrc.java
@@ -0,0 +1,19 @@
+package ru.kireev.ehcache;
+
+import java.util.concurrent.TimeUnit;
+
+public class SlowDataSrc {
+
+    private SlowDataSrc() {
+    }
+
+    static long getValue(int key) {
+        try {
+            Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        return key;
+    }
+
+}
